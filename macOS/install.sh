48b4bf5ecfc4080335307b703acc6e694e8c7808
adam wozniak <9432438+thewozniak@users.noreply.github.com>
2022-12-11 18:43:56 +0100

    Update install.sh

--- macOS/install.sh
+++ macOS/install.sh
@@ -6,12 +6,6 @@ read -p "Do you want to prepare the development environment? [y/n] " answer
 
 if [ "$answer" != "${answer#[Yy]}" ]; then
 
-# Ask for the administrator password upfront
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until `set-default.sh` has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
 # Determine the machine hardware name
 machine=$(uname -m)
 
