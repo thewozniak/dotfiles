e964179669d0e29a700e7d42d550e1993417bd2a
adam wozniak <9432438+thewozniak@users.noreply.github.com>
2022-12-11 17:06:46 +0100

    Update set-defaults.sh

--- macOS/set-defaults.sh
+++ macOS/set-defaults.sh
@@ -6,7 +6,7 @@ osascript -e 'tell application "System Preferences" to quit'
 # Ask for the administrator password upfront
 sudo -v
 
-# Keep-alive: update existing `sudo` time stamp until `.mac_default.sh` has finished
+# Keep-alive: update existing `sudo` time stamp until `set-default.sh` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Ask the user for the computer name
