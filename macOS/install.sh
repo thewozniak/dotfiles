61279e4879db42e796be50ffcf43e3cf6e0271b0
adam wozniak <9432438+thewozniak@users.noreply.github.com>
2023-01-06 12:44:23 +0100

    Update install.sh

--- macOS/install.sh
+++ macOS/install.sh
@@ -94,28 +94,6 @@ brew list node || brew install node
 # Add installed package to the array
 database+=("NodeJS (npm)")
 
-# Create function nginxssl
-function nginxssl() {
-    wget https://woz.ooo/dl/dotfiles/macOS/nginx-server-template.conf -O /usr/local/etc/nginx/servers/$1.conf 
-    sed -i '' "s:{{host}}:$1:" /usr/local/etc/nginx/servers/$1.conf
-    if [ "$2" = "host" ]; then
-      sed  -i '' "s:{{root}}:${HOME}/Sites/$1:" /usr/local/etc/nginx/servers/$1.conf
-      mkdir ${HOME}/Sites/$1
-      sudo chmod -R 775 ${HOME}/Sites/$1
-    else
-      sed  -i '' "s:{{root}}:${HOME}/Sites:" /usr/local/etc/nginx/servers/$1.conf
-    fi
-    openssl req \
-        -x509 -sha256 -nodes -newkey rsa:2048 -days 3650 \
-        -subj "/CN=$1" \
-        -reqexts SAN \
-        -extensions SAN \
-        -config <(cat /System/Library/OpenSSL/openssl.cnf; printf "[SAN]\nsubjectAltName=DNS:$1") \
-        -keyout /usr/local/etc/nginx/ssl/$1.key \
-        -out /usr/local/etc/nginx/ssl/$1.crt
-    sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain /usr/local/etc/nginx/ssl/$1.crt
-}
-
 # Determine the location of the php.ini file
 php_ini=$(php -r "phpinfo();" | grep php.ini | cut -d' ' -f5)
 # Output e.g:
