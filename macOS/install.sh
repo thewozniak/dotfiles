4f99f3ff928167ada5a4158890ace3ea38ca835f
adam wozniak <9432438+thewozniak@users.noreply.github.com>
2023-01-06 15:55:45 +0100

    Update install.sh

--- macOS/install.sh
+++ macOS/install.sh
@@ -15,23 +15,6 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until code is finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-function nginxssl() {
-    # Ask for the administrator password upfront
-    wget https://woz.ooo/dl/dotfiles/macOS/nginx-server-template.conf -O /usr/local/etc/nginx/servers/$1.conf
-    perl -i -pe "s:{{host}}:$1:; s:{{root}}:${HOME}/Sites/$1:" /usr/local/etc/nginx/servers/$1.conf
-    mkdir ${HOME}/Sites/$1
-    sudo chmod -R 775 ${HOME}/Sites/$1
-    openssl req \
-        -x509 -sha256 -nodes -newkey rsa:2048 -days 3650 \
-        -subj "/CN=$1" \
-        -reqexts SAN \
-        -extensions SAN \
-        -config <(cat /System/Library/OpenSSL/openssl.cnf; printf "[SAN]\nsubjectAltName=DNS:$1") \
-        -keyout /usr/local/etc/nginx/ssl/$1.key \
-        -out /usr/local/etc/nginx/ssl/$1.crt
-    sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain /usr/local/etc/nginx/ssl/$1.crt  
-}
-
 # Create an empty array named 'database' to add information about installed packages
 database=()
 
@@ -231,10 +214,6 @@ sudo chmod -R 644 ${HOME}/Sites/php-info.php
 # Change the group for the directory and files
 chgrp -R -f staff ${HOME}/Sites
 
-# Create and add SSL certificates for hosts
-nginxssl localhost
-nginxssl dev.mac
-
 # Reload nginx service as a root
 sudo nginx -s reload
 database+=("Nginx Web Server")
@@ -307,6 +286,13 @@ done
 # Download the .zshrc file to your home directory
 rm -rf ${HOME}/.zshrc
 curl https://raw.githubusercontent.com/thewozniak/dotfiles/main/macOS/.zshrc -o ~/.zshrc
+# Run .zshrc
+bash ~/.zshrc
+# Create and add SSL certificates for hosts
+nginxssl localhost
+nginxssl dev.mac
+# Reload nginx
+sudo nginx -s reload
 echo # empty line ;)
 echo "\033[1mThe following packages and libraries have been installed:\033[0m"
 for item in "${database[@]}"
